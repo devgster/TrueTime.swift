a4fbb85287638d1918f3f2089978fd08fd475a95
박경우 <devgster@gmail.com>
2024-12-13 15:58:39 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,6 +18,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TrueTime",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources", 
+        ),
     ]
 )
